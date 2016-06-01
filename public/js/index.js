f46390a4f85d7eace2cb5791ce618fdc85ae612a
haojieguangxin <haojieguangxin@163.com>
2016-06-01 14:28:49 +0800

    git测试

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhang-j on 2016/3/21.
+ */
+new Vue({
+    el: '#app',
+    data: {
+        message: 'hello world'
+    }
+});
---
+++ public/js/list.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhang-j on 2016/3/21.
+ */
+new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {text: 'Learn JavaScript'},
+            {text: 'Learn vuejs'}
+        ]
+    }
+});
